80d7bafa9a6637b9a7a44c3f14df1978c79b0e2a
georgetreger <georgetreger@gmail.com>
2023-10-08 08:11:25 +0100

    update files

---
+++ project2/app.js
@@ -0,0 +1,11 @@
+
+
+const faEl = document.querySelector('.fa-bars');
+
+const menuEl = document.querySelector('.menu');
+
+faEl.addEventListener('click', function(){
+    menuEl.classList.toggle('show-menu')
+    console.log('hi');
+})
+      
