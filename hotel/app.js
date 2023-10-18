db93aaecbc99f82df5f2dafb4758f1a8e6f50479
georgetreger <georgetreger@gmail.com>
2023-10-18 09:21:25 +0100

    changes made

--- hotel/app.js
+++ hotel/app.js
@@ -1,27 +1,27 @@
 const hall = [
      {
        id: 1,
-       name: 'susan smith',
+       name: '',
        img: 'https://www.course-api.com/images/people/person-1.jpeg',
-       text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+       text: " ",
      },
      {
        id: 2,
-       name: 'anna johnson',
+       name: '',
        img: 'https://www.course-api.com/images/people/person-2.jpeg',
-       text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+       text: '.',
      },
      {
        id: 3,
-       name: 'peter jones',
+       name: '',
        img: 'https://www.course-api.com/images/people/person-4.jpeg',
-       text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+       text: ' .',
      },
      {
        id: 4,
-       name: 'bill anderson',
+       name: '',
        img: 'https://www.course-api.com/images/people/person-3.jpeg',
-       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+       text: ' . ',
      },
    ];
 
